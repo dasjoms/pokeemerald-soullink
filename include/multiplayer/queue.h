a32094f8c2d96cf1bf253ca9eb44f9f6ed05c34a
dasjoms <36546056+dasjoms@users.noreply.github.com>
2026-03-06 07:19:25 +0100

    Add priority-aware multiplayer queue API and overflow policies

--- include/multiplayer/queue.h
+++ include/multiplayer/queue.h
@@ -5,7 +5,15 @@
 
 #define MULTIPLAYER_QUEUE_CAPACITY 16
 
-struct MultiplayerQueue
+enum MultiplayerQueuePriority
+{
+    MULTIPLAYER_QUEUE_PRIORITY_LOW,
+    MULTIPLAYER_QUEUE_PRIORITY_NORMAL,
+    MULTIPLAYER_QUEUE_PRIORITY_HIGH,
+    MULTIPLAYER_QUEUE_PRIORITY_COUNT,
+};
+
+struct MultiplayerQueueRing
 {
     u16 head;
     u16 tail;
@@ -13,8 +21,20 @@ struct MultiplayerQueue
     struct MultiplayerPacket entries[MULTIPLAYER_QUEUE_CAPACITY];
 };
 
+struct MultiplayerQueue
+{
+    struct MultiplayerQueueRing rings[MULTIPLAYER_QUEUE_PRIORITY_COUNT];
+    u16 count;
+    u32 overflowCount;
+    u32 overflowByPriority[MULTIPLAYER_QUEUE_PRIORITY_COUNT];
+};
+
 void MultiplayerQueue_Init(struct MultiplayerQueue *queue);
-bool32 MultiplayerQueue_Enqueue(struct MultiplayerQueue *queue, const struct MultiplayerPacket *packet);
-bool32 MultiplayerQueue_Dequeue(struct MultiplayerQueue *queue, struct MultiplayerPacket *packet);
+void MultiplayerQueue_Reset(struct MultiplayerQueue *queue);
+bool32 MultiplayerQueue_Push(struct MultiplayerQueue *queue, const struct MultiplayerPacket *packet, u8 priority);
+bool32 MultiplayerQueue_PopNext(struct MultiplayerQueue *queue, struct MultiplayerPacket *packet);
+u16 MultiplayerQueue_GetCount(const struct MultiplayerQueue *queue);
+u32 MultiplayerQueue_GetOverflowCount(const struct MultiplayerQueue *queue);
+u32 MultiplayerQueue_GetOverflowCountByPriority(const struct MultiplayerQueue *queue, u8 priority);
 
 #endif // GUARD_MULTIPLAYER_QUEUE_H
